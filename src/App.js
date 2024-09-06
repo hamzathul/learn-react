b2efb433e70006d9567d4f2c7b171dd986385574
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-06 14:26:09 +0530

    Fresh Project

--- src/App.js
+++ src/App.js
@@ -4,35 +4,12 @@ import Header from './components/Header'
 import Employee from './Employee';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const addCount=()=>{
-    setCount(count+1)
-  }
-  let emp = [{
-    name:'Ganesh',
-    age:21,
 
-  },
-  {
-    name:'Arun',
-    age:31,
-    
-  },
-  {
-    name:'Rajesh',
-    age:42,
-    
-  }]
   return (
-    <div className='App'>
-      {
-        emp.map((obj, index)=>
-          (
-            <Employee key={index} {...obj}></Employee>
-          )
-        )
-      }
-    </div>
+  <div className='App'>
+    <h1>Hello World</h1>
+
+  </div>
   );
 }
 
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react"
-
-class Header extends Component{
-    render(){
-        return(<h1>Hello I am {this.props.data}</h1>)
-    }
-}
-
-export default Header
