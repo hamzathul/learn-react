5faafa4de63ac3c3f67739a7df2154eb061bd746
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-09-06 11:38:50 +0530

    Adding new changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { useState } from 'react';
 import './App.css'
 import Header from './components/Header'
+import Employee from './Employee';
 
 function App() {
-  const data = 'what are you doing?'
+  const [count, setCount] = useState(0)
+  const addCount=()=>{
+    setCount(count+1)
+  }
+  let emp = [{
+    name:'Ganesh',
+    age:21,
+
+  },
+  {
+    name:'Arun',
+    age:31,
+    
+  },
+  {
+    name:'Rajesh',
+    age:42,
+    
+  }]
   return (
-    <div>
-      <Header data = {data}/>
-      <h1 className='hello'>Hello world</h1>
-      {/* <p>This is sample {data}</p> */}
+    <div className='App'>
+      {
+        emp.map((obj, index)=>
+          (
+            <Employee key={index} {...obj}></Employee>
+          )
+        )
+      }
     </div>
   );
-}
-
-function Hello(){
-  return(<h1 className='hello'>Hello World</h1>)
 }
 
 export default App;
